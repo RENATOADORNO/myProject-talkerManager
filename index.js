ac73f856f3a9d99b40108ff30cd5298b14deeab1
Renato Adorno <renatowillian.2000@gmail.com>
2022-03-21 22:52:17 -0300

    :sparkles: feat(1): criando o endpoint GET /talker

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs/promises');
 
+const talker = 'talker.json';
 const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+
+app.get('/talker', async (req, res) => {
+  try {
+    const data = await fs.readFile(talker);
+    res.status(200).json(JSON.parse(data));
+  } catch (e) {
+    res.status(500).send({ message: 'erro no servidor' });
+  }
+});
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
