4630d2d7e23278b1a0651320a3acfcd5aeea3536
Renato Adorno <renatowillian.2000@gmail.com>
2022-03-26 19:36:46 -0300

    :recycle: refactor(1/2) Refatorando o end point Talker

--- index.js
+++ index.js
@@ -1,36 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const fs = require('fs/promises');
+const talkerController = require('./src/controller/talker');
 
-const talker = 'talker.json';
 const app = express();
-app.use(bodyParser.json());
-
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
-app.get('/talker', async (req, res) => {
-  try {
-    const data = await fs.readFile(talker);
-    res.status(200).json(JSON.parse(data));
-  } catch (e) {
-    res.status(500).send({ message: 'erro no servidor' });
-  }
-});
+app.use(bodyParser.json());
+app.use(express.json());
 
-app.get('/talker/:id', async (req, res) => {
-  const { id } = req.params;
-
-  fs.readFile(talker)
-  .then((data) => {
-    const dataId = JSON.parse(data).find((item) => item.id === Number(id));
-    if (dataId) {
-      return res.status(200).json(dataId);
-    }
-    return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
-  })
-  .catch((_err) => res.status(500).send({ message: 'erro no servidor' }));
-});
+app.use('/talker', talkerController);
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
---
+++ src/controller/talker.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const fs = require('fs/promises');
+
+const router = express.Router();
+
+const talker = 'talker.json';
+
+router.get('/', async (_req, res) => {
+  try {
+    const data = await fs.readFile(talker);
+    res.status(200).json(JSON.parse(data));
+  } catch (e) {
+    res.status(500).send({ message: 'erro no servidor' });
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+
+  fs.readFile(talker)
+  .then((data) => {
+    const dataId = JSON.parse(data).find((item) => item.id === +id);
+    if (dataId) {
+      return res.status(200).json(dataId);
+    }
+    return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  })
+  .catch((_err) => res.status(500).send({ message: 'erro no servidor' }));
+});
+
+module.exports = router;
