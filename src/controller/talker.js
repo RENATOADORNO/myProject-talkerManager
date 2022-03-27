7b4b07adea13a37e8bccad498ba4dc482b203538
Renato Adorno <renatowillian.2000@gmail.com>
2022-03-26 22:10:41 -0300

    :sparkles: feat(4) Criando o endpoint POST /talker

--- src/controller/talker.js
+++ src/controller/talker.js
@@ -28,8 +28,124 @@ router.get('/:id', async (req, res) => {
   .catch((_err) => res.status(500).send({ message: 'erro no servidor' }));
 });
 
-// router.post('/', async (req, res) => {
+const auth = (req, res, next) => {
+  const { authorization } = req.headers;
 
-// });
+  if (!authorization) {
+    return res.status(401).send({ message: 'Token não encontrado' });
+  }
+
+  if (authorization.length !== 16) {
+    return res.status(401).send({ message: 'Token inválido' });
+  }
+
+  next();
+};
+
+const talkerName = (req, res, next) => {
+  const { name } = req.body;
+
+  const nameNotFound = { message: 'O campo "name" é obrigatório' };
+  const nameTooShort = { message: 'O "name" deve ter pelo menos 3 caracteres' };
+
+  if (!name || name === '') res.status(400).json(nameNotFound);
+
+  if (name && name.length < 3) res.status(400).json(nameTooShort);
+
+  next();
+};
+
+const talkerAge = (req, res, next) => {
+  const { age } = req.body;
+
+  const ageNotFound = { message: 'O campo "age" é obrigatório' };
+  const talkerTooYoung = { message: 'A pessoa palestrante deve ser maior de idade' };
+
+  if (!age || age === '') res.status(400).json(ageNotFound);
+
+  if (Number(age) < 18) res.status(400).json(talkerTooYoung);
+
+  next();
+};
+
+const talkerObj = (req, res, next) => {
+  const { talk } = req.body;
+
+  const talkNotFound = {
+    message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios',
+  };
+
+  if (!talk) res.status(400).json(talkNotFound);
+
+  next();
+};
+
+const talkerTalk = (req, res, next) => {
+  const { talk: { watchedAt, rate } } = req.body;
+
+  const talkNotFound = {
+    message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios',
+  };
+
+  if ((!watchedAt || watchedAt === '')
+    || (!rate || rate === '')) {
+    res.status(400).json(talkNotFound);
+  }
+
+  next();
+};
+
+const talkerDate = (req, res, next) => {
+  const { talk: { watchedAt } } = req.body;
+  const date = watchedAt.split('/');
+
+  const wrongDate = { message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' };
+
+  if (date.length !== 3
+    || Number(date[0]) > 31
+    || Number(date[1]) > 12) {
+    res.status(400).json(wrongDate);
+  }
+
+  next();
+};
+
+const talkerRate = (req, res, next) => {
+  const { talk: { rate } } = req.body;
+
+  const rateWrong = {
+    message: 'O campo "rate" deve ser um inteiro de 1 à 5',
+  };
+
+  if (Number(rate) < 1 || Number(rate) > 5) res.status(400).json(rateWrong);
+
+  next();
+};
+
+router.post('/', 
+  auth, 
+  talkerName, 
+  talkerAge, 
+  talkerObj, 
+  talkerTalk, 
+  talkerDate, 
+  talkerRate,
+
+  async (req, res) => {
+    const { name, age, talk: { watchedAt, rate } } = req.body;
+    fs.readFile(talker, 'utf8')
+      .then(async (data) => {
+        const dataJson = JSON.parse(data);
+        const newId = dataJson[dataJson.length - 1].id + 1;
+        dataJson.push({ name, age, id: newId, talk: { watchedAt, rate } });
+        try {
+          await fs.writeFile(talker, JSON.stringify(dataJson));
+          res.status(201).json({ name, age, id: newId, talk: { watchedAt, rate } });
+        } catch (err) {
+          console.log(err);
+        }
+      })
+      .catch((err) => console.log(err));
+});
 
 module.exports = router;
